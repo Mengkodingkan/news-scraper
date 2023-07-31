36f64a93b14e06134a6c9fae635f15795f17c114
faisalProject <mf494284@gmail.com>
2023-07-31 15:25:23 +0700

    added navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
+// bootstrap
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+// bootstrap icons
+import '../node_modules/bootstrap-icons/font/bootstrap-icons.css';
+
+// css
+import './main.css';
+
 import './App.css';
 
-function App() {
+import Navbar from './component/Navbar';
+import Home from './page/Home';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Navbar></Navbar>
+
+        <Routes>
+          <Route path='/' element={<Home/>}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
