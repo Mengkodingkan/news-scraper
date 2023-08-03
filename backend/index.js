0a4d8982f3cad43474f68c3e2a14558da4e1ede4
KatowProject <naufalfaqih443@gmail.com>
2023-08-03 20:30:06 +0700

    added route content

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/api', require('./routes/main'));
+app.use('/api', require('./routes/main'), require('./routes/content'));
 
 app.get('*', (req, res) => {
     res.json({
