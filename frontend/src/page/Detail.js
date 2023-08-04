b19bc0aa368e028b5e4de80472d0de7d808bf574
rigger02 <akangrigger@gmail.com>
2023-08-04 13:01:10 +0700

    add detail

--- frontend/src/page/Detail.js
+++ frontend/src/page/Detail.js
@@ -1,7 +1,82 @@
+import React from 'react';
+import Sidebar from "../component/Sidebar";
+import LatestNews from "../component/LatestNews";
+import Navbar from '../component/Navbar';
+import Footer from '../component/Footer';
+
 const Detail = () => {
-    return(
-        <></>
-    )
-}
+    return (
+        <>
+            <Navbar></Navbar>
+
+            {/* Main content */}
+            <div className="main-content">
+                {/* Content */}
+                <div className="content">
+                    <nav>
+                        <ul>
+                            <li><a href="/">Beranda {'>'}</a></li>
+                            <li><a href="/metropolis">Metropolis {'>'}</a></li>
+                            <li>Pelajar SMA Menikah tanpa Sepengatahuan Orang Tua</li>
+                        </ul>
+                    </nav>
+                    <h1>Pelajar SMA Menikah tanpa Sepengatahuan Orang Tua</h1>
+                    <p>Penulis Mang Raka - 27 Juli 2023</p>
+                    <img src="#" alt="Pelajar SMA Menikah" />
+                    <p className="image-caption">Pelajar SMA Menikah tanpa Sepengatahuan Orang Tua</p>
+                    <p>
+                    TELUKJAMBE, RAKA- Mahasiswa Ilmu Komunikasi Unsika menggelar buka puasa bersama dengan anak-anak di Taman Baca Sahabat Buku Perumnas Telukjambe Timur.
+                    Cindy (20), salah satu mahasiswa Ilmu Komunikasi Unsika mengatakan, bahwa kegiatan ini merupakan pengimplementasian dari mata kuliah publik relation di kampusnya. “Ini merupakan implementasi dari mata kuliah public relations dan kami memilih di Taman Baca Sahabat Buku,” katanya, kepada Radar Karawang, Senin (25/4).
+                    Cindy menambahkan, bahwa tujuan dirinya bersama rekan-rekannya memilih Taman Baca Sahabat Buku ialah untuk berbagi ilmu dengan anak-anak yang memang berada di wilayah sekretariat Taman Baca Sahabat Buku. “Tujuannya sih kami ingin membantu pendiri Taman Baca Sahabat Buku untuk mengenalkan literasi kepada anak-anak disekitar taman bacanya, ” tambahnya.
+                    Perempuan asal Bandung ini pun menuturkan, selain memberikan bantuan buku. Dirinya dan teman-teman kelompoknya mengadakan kegiatan games dan perlombaan. “Selain memberikan buku untuk menambah literasi bacaan, kami juga mengadakan perlombaan mewarnai untuk anak-anak,” tuturnya.
+                    Sementara itu, pengelola Taman Baca Sahabat Buku menerangkan bahwa ini merupakan kali pertama mahasiswa membuat kegiatan di Taman Baca Sahabat Buku. “Saya ucapkan terima kasih banyak kepada teman-teman mahasiswa. Ini kali pertama mahasiswa mengadakan kegiatan di sini,” pungkasnya. (cr8)
+                    TELUKJAMBE, RAKA- Mahasiswa Ilmu Komunikasi Unsika menggelar buka puasa bersama dengan anak-anak di Taman Baca Sahabat Buku Perumnas Telukjambe Timur.
+                    Cindy (20), salah satu mahasiswa Ilmu Komunikasi Unsika mengatakan, bahwa kegiatan ini merupakan pengimplementasian dari mata kuliah publik relation di kampusnya. “Ini merupakan implementasi dari mata kuliah public relations dan kami memilih di Taman Baca Sahabat Buku,” katanya, kepada Radar Karawang, Senin (25/4).
+                    Cindy menambahkan, bahwa tujuan dirinya bersama rekan-rekannya memilih Taman Baca Sahabat Buku ialah untuk berbagi ilmu dengan anak-anak yang memang berada di wilayah sekretariat Taman Baca Sahabat Buku. “Tujuannya sih kami ingin membantu pendiri Taman Baca Sahabat Buku untuk mengenalkan literasi kepada anak-anak disekitar taman bacanya, ” tambahnya.
+                    Perempuan asal Bandung ini pun menuturkan, selain memberikan bantuan buku. Dirinya dan teman-teman kelompoknya mengadakan kegiatan games dan perlombaan. “Selain memberikan buku untuk menambah literasi bacaan, kami juga mengadakan perlombaan mewarnai untuk anak-anak,” tuturnya.
+                    Sementara itu, pengelola Taman Baca Sahabat Buku menerangkan bahwa ini merupakan kali pertama mahasiswa membuat kegiatan di Taman Baca Sahabat Buku. “Saya ucapkan terima kasih banyak kepada teman-teman mahasiswa. Ini kali pertama mahasiswa mengadakan kegiatan di sini,” pungkasnya. (cr8)
+                    TELUKJAMBE, RAKA- Mahasiswa Ilmu Komunikasi Unsika menggelar buka puasa bersama dengan anak-anak di Taman Baca Sahabat Buku Perumnas Telukjambe Timur.
+                    Cindy (20), salah satu mahasiswa Ilmu Komunikasi Unsika mengatakan, bahwa kegiatan ini merupakan pengimplementasian dari mata kuliah publik relation di kampusnya. “Ini merupakan implementasi dari mata kuliah public relations dan kami memilih di Taman Baca Sahabat Buku,” katanya, kepada Radar Karawang, Senin (25/4).
+                    Cindy menambahkan, bahwa tujuan dirinya bersama rekan-rekannya memilih Taman Baca Sahabat Buku ialah untuk berbagi ilmu dengan anak-anak yang memang berada di wilayah sekretariat Taman Baca Sahabat Buku. “Tujuannya sih kami ingin membantu pendiri Taman Baca Sahabat Buku untuk mengenalkan literasi kepada anak-anak disekitar taman bacanya, ” tambahnya.
+                    Perempuan asal Bandung ini pun menuturkan, selain memberikan bantuan buku. Dirinya dan teman-teman kelompoknya mengadakan kegiatan games dan perlombaan. “Selain memberikan buku untuk menambah literasi bacaan, kami juga mengadakan perlombaan mewarnai untuk anak-anak,” tuturnya.
+                    Sementara itu, pengelola Taman Baca Sahabat Buku menerangkan bahwa ini merupakan kali pertama mahasiswa membuat kegiatan di Taman Baca Sahabat Buku. “Saya ucapkan terima kasih banyak kepada teman-teman mahasiswa. Ini kali pertama mahasiswa mengadakan kegiatan di sini,” pungkasnya. (cr8)
+                    </p>
+                    <hr />
+                    <div className="navigation-buttons">
+                        <button>Berita Sebelumnya...</button>
+                        <button>Berita Berikutnya...</button>
+                    </div>
+                    <div className="author">
+                        <div className="author-image">
+                            <img src="penulis.jpg" alt="Mang Raka" />
+                        </div>
+                        <div className="author-info">
+                            <p>Mang Raka</p>
+                            <a href="https://radarkarawang.id">https://radarkarawang.id</a>
+                            <div className="social-media">
+                                <a href="#">Facebook</a>
+                                <a href="#">Twitter</a>
+                            </div>
+                        </div>
+                    </div>
+                    <hr/>
+                </div>
+
+                {/* Latest News */}
+                <div className="latest-news">
+                    <hr/>
+                    <LatestNews />
+                </div>
+            </div>
+
+            {/* Footer */}
+            <footer>
+                <Footer />
+            </footer>
+
+            <Sidebar />
+        </>
+    );
+};
 
 export default Detail;
